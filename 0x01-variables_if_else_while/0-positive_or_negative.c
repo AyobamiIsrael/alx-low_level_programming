7d75173b5849793cf9833938fdcf7a584b6f7404
AyobamiIsrael <olunrebiayobami@gmail.com>
2022-06-06 13:26:30 -0700

    C programming language on if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,3 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -21,12 +16,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+	{	
 		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	}
+	else if (n == 0)
+	{	
 		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 
-
+	}
 	return (0);
 }
